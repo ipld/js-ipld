46914adaa81d57770930757e492343a760db118b
David Dias <daviddias.p@gmail.com>
2017-02-07 20:52:13 -0800

    test: refactor eth-block tests

--- test/ipld-eth-block.js
+++ test/ipld-eth-block.js
@@ -11,126 +11,7 @@ const pull = require('pull-stream')
 const IPLDResolver = require('../src')
 
 module.exports = (repo) => {
-  describe.skip('IPLD Resolver with eth-block (Ethereum Block)', () => {
-    const bs = new BlockService(repo)
-    const resolver = new IPLDResolver(bs)
-
-    let node1
-    let node2
-    let node3
-    let cid1
-    let cid2
-    let cid3
-
-    before((done) => {
-      node1 = new EthBlockHeader({ number: 1 })
-      node2 = new EthBlockHeader({ number: 2 })
-      node3 = new EthBlockHeader({ number: 3 })
-
-      series([
-        (cb) => {
-          ipldEthBlock.util.cid(node1, (err, cid) => {
-            expect(err).to.not.exist
-            cid1 = cid
-            cb()
-          })
-        },
-        (cb) => {
-          ipldEthBlock.util.cid(node2, (err, cid) => {
-            expect(err).to.not.exist
-            cid2 = cid
-            cb()
-          })
-        },
-        (cb) => {
-          ipldEthBlock.util.cid(node3, (err, cid) => {
-            expect(err).to.not.exist
-            cid3 = cid
-            cb()
-          })
-        }
-      ], done)
-    })
-
-    it('resolver.put', (done) => {
-      resolver.put({
-        node: node1,
-        cid: cid1
-      }, done)
-    })
-
-    it('resolver.putStream', (done) => {
-      pull(
-        pull.values([
-          { node: node1, cid: cid1 },
-          { node: node2, cid: cid2 },
-          { node: node3, cid: cid3 }
-        ]),
-        resolver.putStream(done)
-      )
-    })
-
-    it('resolver.get', (done) => {
-      resolver.put({
-        node: node1,
-        cid: cid1
-      }, (err) => {
-        expect(err).to.not.exist
-        resolver.get(cid1, (err, node) => {
-          expect(err).to.not.exist
-          expect(node1.number.toString('hex')).to.eql('01')
-          expect(node1.raw).to.eql(node.raw)
-          expect(node1.hash()).to.eql(node.hash())
-          done()
-        })
-      })
-    })
-
-    it('resolver.getStream', (done) => {
-      resolver.put({
-        node: node1,
-        cid: cid1
-      }, (err) => {
-        expect(err).to.not.exist
-        pull(
-          resolver.getStream(cid1),
-          pull.collect((err, nodes) => {
-            expect(err).to.not.exist
-            expect(node1.raw).to.eql(nodes[0].raw)
-            expect(node1.hash()).to.eql(nodes[0].hash())
-            done()
-          })
-        )
-      })
-    })
-
-    it('resolver.remove', (done) => {
-      resolver.put({
-        node: node1,
-        cid: cid1
-      }, (err) => {
-        expect(err).to.not.exist
-        resolver.get(cid1, (err, node) => {
-          expect(err).to.not.exist
-          expect(node1.raw).to.eql(node.raw)
-          expect(node1.hash()).to.eql(node.hash())
-          remove()
-        })
-      })
-
-      function remove () {
-        resolver.remove(cid1, (err) => {
-          expect(err).to.not.exist
-          resolver.get(cid1, (err) => {
-            expect(err).to.exist
-            done()
-          })
-        })
-      }
-    })
-  })
-
-  describe.skip('IPLD Path Resolver for eth-block', () => {
+  describe('IPLD Resolver with eth-block (Ethereum Block)', () => {
     let resolver
 
     let node1
@@ -141,7 +22,8 @@ module.exports = (repo) => {
     let cid3
 
     before((done) => {
-      resolver = new IPLDResolver()
+      const bs = new BlockService(repo)
+      resolver = new IPLDResolver(bs)
 
       series([
         (cb) => {
@@ -188,44 +70,116 @@ module.exports = (repo) => {
             { node: node2, cid: cid2 },
             { node: node3, cid: cid3 }
           ]),
-          resolver.putStream(done)
+          pull.asyncMap((nac, cb) => resolver.put(nac.node, nac.cid, cb)),
+          pull.onEnd(done)
         )
       }
     })
 
-    it('root path (same as get)', (done) => {
-      resolver.resolve(cid1, '/', (err, result) => {
-        expect(err).to.not.exist
+    describe('internals', () => {
+      it('resolver._putStream', (done) => {
+        pull(
+          pull.values([
+            { node: node1, cid: cid1 },
+            { node: node2, cid: cid2 },
+            { node: node3, cid: cid3 }
+          ]),
+          resolver._putStream(done)
+        )
+      })
 
-        ipldEthBlock.util.cid(result, (err, cid) => {
+      it('resolver._get', (done) => {
+        resolver.put(node1, cid1, (err) => {
           expect(err).to.not.exist
-          expect(cid).to.eql(cid1)
-          done()
+          resolver.get(cid1, (err, result) => {
+            expect(err).to.not.exist
+            expect(node1.number.toString('hex')).to.eql('01')
+            expect(node1.raw).to.eql(result.value.raw)
+            expect(node1.hash()).to.eql(result.value.hash())
+            done()
+          })
+        })
+      })
+
+      it('resolver._getStream', (done) => {
+        resolver.put(node1, cid1, (err) => {
+          expect(err).to.not.exist
+          pull(
+            resolver._getStream(cid1),
+            pull.collect((err, results) => {
+              expect(err).to.not.exist
+              const node = results[0]
+              expect(node1.raw).to.eql(node.raw)
+              expect(node1.hash()).to.eql(node.hash())
+              done()
+            })
+          )
         })
       })
     })
 
-    it('value within 1st node scope', (done) => {
-      resolver.resolve(cid1, 'number', (err, result) => {
-        expect(err).to.not.exist
-        expect(result.toString('hex')).to.eql('01')
-        done()
+    describe('public api', () => {
+      it('resolver.put', (done) => {
+        resolver.put(node1, cid1, done)
       })
-    })
 
-    it('value within nested scope (1 level)', (done) => {
-      resolver.resolve(cid2, 'parent/number', (err, result) => {
-        expect(err).to.not.exist
-        expect(result.toString('hex')).to.eql('01')
-        done()
+      it('root path (same as get)', (done) => {
+        resolver.get(cid1, '/', (err, result) => {
+          expect(err).to.not.exist
+
+          ipldEthBlock.util.cid(result.value, (err, cid) => {
+            expect(err).to.not.exist
+            expect(cid).to.eql(cid1)
+            done()
+          })
+        })
       })
-    })
 
-    it('value within nested scope (2 levels)', (done) => {
-      resolver.resolve(cid3, 'parent/parent/number', (err, result) => {
-        expect(err).to.not.exist
-        expect(result.toString('hex')).to.eql('01')
-        done()
+      it('value within 1st node scope', (done) => {
+        resolver.get(cid1, 'number', (err, result) => {
+          expect(err).to.not.exist
+          expect(result.value.toString('hex')).to.eql('01')
+          done()
+        })
+      })
+
+      it('value within nested scope (1 level)', (done) => {
+        resolver.get(cid2, 'parent/number', (err, result) => {
+          expect(err).to.not.exist
+          expect(result.value.toString('hex')).to.eql('01')
+          done()
+        })
+      })
+
+      it('value within nested scope (2 levels)', (done) => {
+        resolver.get(cid3, 'parent/parent/number', (err, result) => {
+          expect(err).to.not.exist
+          expect(result.value.toString('hex')).to.eql('01')
+          done()
+        })
+      })
+
+      it('resolver.remove', (done) => {
+        resolver.put(node1, cid1, (err) => {
+          expect(err).to.not.exist
+          resolver.get(cid1, (err, result) => {
+            expect(err).to.not.exist
+            const node = result.value
+            expect(node1.raw).to.eql(node.raw)
+            expect(node1.hash()).to.eql(node.hash())
+            remove()
+          })
+        })
+
+        function remove () {
+          resolver.remove(cid1, (err) => {
+            expect(err).to.not.exist
+            resolver.get(cid1, (err) => {
+              expect(err).to.exist
+              done()
+            })
+          })
+        }
       })
     })
   })
