be77abd23da39c173c09486e82ed76ffca9a4a4c
Volker Mische <volker.mische@gmail.com>
2018-09-26 16:07:56 +0200

    refactor: remove eth-hash-to-cid module
    The eth-hash-to-cid is a module that is no longer used by any
    project. Hence replace it with two simple lines of code.

--- test/ipld-eth.js
+++ test/ipld-eth.js
@@ -11,9 +11,10 @@ const ipldEthBlock = require('ipld-ethereum').ethBlock
 const ipldEthStateTrie = require('ipld-ethereum').ethStateTrie
 const loadFixture = require('aegir/fixtures')
 const async = require('async')
-const cidForHash = require('eth-hash-to-cid')
 const EthBlockHeader = require('ethereumjs-block/header')
 const EthTrieNode = require('merkle-patricia-tree/trieNode')
+const multihashes = require('multihashes')
+const CID = require('cids')
 
 const IPLDResolver = require('../src')
 
@@ -76,10 +77,12 @@ module.exports = (repo) => {
           default: throw new Error('Unknown type!')
         }
 
+        const multihash = multihashes.encode(node.hash(), 'keccak-256')
+        const cid = new CID(1, type, multihash)
         return {
           raw: rawData,
           node: node,
-          cid: cidForHash(type, node.hash())
+          cid: cid
         }
       }
 
